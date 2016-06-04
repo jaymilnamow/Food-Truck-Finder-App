eca477c8a173910e6fda5992b203e4369fe83a80
Jay Milnamow <jaymilnamow@Zipcoder29s-MacBook-Pro.local>
2016-06-04 15:58:52 -0400

    Best Food Truck Finder EVER.

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope) {
+  init = function(){
+    console.log("STC");
+  }
+  init();
+})
+
+.controller('VendorsCtrl', function($scope) {
+  init = function(){
+    console.log("ANC");
+  }
+  init();
+})
+
+.controller('KapowCtrl', function($scope) {
+  init = function(){
+    console.log("SC");
+  }
+  init();
+})
